1b199b42a5cc7fe473d35c24273f6e07cf2899e8
willthepan <williampan.us@gmail.com>
2026-04-24 05:05:55 +0000

    final

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,33 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int minSteps;
+    private int totalSteps;
+    private int dayCount;
+    private int activeDays;
+
+
+    public StepTracker(int activityThreshold) {
+        this.minSteps = activityThreshold;
+        totalSteps = 0;
+        dayCount = 0;
+        activeDays=0;
+    
+    }
+    void addDailySteps(int stepCount) {
+        totalSteps += stepCount;
+        dayCount++;
+        if(stepCount >= minSteps){
+            activeDays++;
+        }
+    }
+    int activeDays() {
+        return activeDays;
+    }
+    double averageSteps() {
+        if(dayCount==0){
+            return 0; 
+        }
+        return (double)totalSteps/dayCount;
+    }
 }
